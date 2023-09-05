05408ec0913870152cb5bb8c16f98bb2fe7c108a
Muhammad Ismail <muhammad.ismail15604@gmail.com>
2023-09-05 21:01:37 +0500

    next task 1) check box 2) booking page improvement 3) in customer dashboard username is not set uniquely Task complete all pages except above

--- lib/CustomerPannels/Booking.dart
+++ lib/CustomerPannels/Booking.dart
@@ -1,7 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fashionzone/Components/BottomNavigationBarComponent.dart';
+import 'package:fashionzone/Components/NearYouComponent.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import '../Components/AppBarComponent.dart';
 import '../Components/DrawerComponent.dart';
 import 'Thanks.dart';
@@ -16,10 +19,17 @@ class Booking extends StatefulWidget {
 class _BookingState extends State<Booking> {
   final formKey = GlobalKey<FormState>();
   final nameController = TextEditingController();
+  final emailController = TextEditingController();
   final phoneController = TextEditingController();
   final addressController = TextEditingController();
   final timeController = TextEditingController();
-  bool isHover = false;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchAndSetUserData();
+  }
+
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
@@ -32,32 +42,15 @@ class _BookingState extends State<Booking> {
       ),
       home: Scaffold(
         backgroundColor: const Color.fromARGB(224, 248, 249, 252),
-        appBar: const MyCustomAppBarComponent(),
+        appBar: const MyCustomAppBarComponent(appBarTitle: 'Booking'),
         drawer: const MyCustomDrawerComponent(),
-        body: Form(
-          autovalidateMode: AutovalidateMode.onUserInteraction,
-          key: formKey,
-          child: Center(
+        body: Center(
+          child: Form(
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            key: formKey,
             child: SingleChildScrollView(
               child: Column(
                 children: [
-                  // image
-                  Padding(
-                    padding: EdgeInsets.only(
-                      top: screenHeight * 0.1,
-                    ),
-                    child: SizedBox(
-                      child: Image(
-                        image: const AssetImage('images/logo.png'),
-                        height: MediaQuery.of(context).size.height * 0.25,
-                        width: MediaQuery.of(context).size.width * 0.25,
-                      ),
-                    ),
-                  ),
-                  //space
-                  const SizedBox(
-                    height: 20,
-                  ),
                   // full name
                   SizedBox(
                     height: 50,
@@ -91,7 +84,7 @@ class _BookingState extends State<Booking> {
                               ),
                               border: OutlineInputBorder()),
                           style: const TextStyle(
-                              fontFamily: 'Poppins', fontSize: 20),
+                              fontFamily: 'Poppins', fontSize: 16),
                           validator: (value) {
                             if (value!.isEmpty) {
                               return 'Please enter your full name.';
@@ -108,6 +101,63 @@ class _BookingState extends State<Booking> {
                   const SizedBox(
                     height: 20,
                   ),
+                  //email
+                  SizedBox(
+                    height: 50,
+                    width: MediaQuery.of(context).size.width *
+                        0.86,
+                    child: Material(
+                      elevation: 5,
+                      shadowColor: Colors.black,
+                      child: TextFormField(
+                          controller: emailController,
+                          keyboardType:
+                          TextInputType.emailAddress,
+                          decoration: const InputDecoration(
+                              fillColor: Colors.white,
+                              filled: true,
+                              labelText: 'Email',
+                              labelStyle: TextStyle(
+                                  fontFamily: 'Poppins',
+                                  fontSize: 16,
+                                  color: Color.fromARGB(
+                                      247, 84, 74, 158)),
+                              prefixIcon: Icon(
+                                Icons.email,
+                                color: Color.fromARGB(
+                                    247, 84, 74, 158),
+                              ),
+                              enabledBorder:
+                              OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Colors.white),
+                              ),
+                              focusedBorder:
+                              OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Color.fromARGB(
+                                        247, 84, 74, 158),
+                                    width: 1),
+                              ),
+                              border: OutlineInputBorder()),
+                          style: const TextStyle(
+                              fontFamily: 'Poppins',
+                              fontSize: 16),
+                          validator: (value) {
+                            if (value!.isEmpty) {
+                              return 'Please enter your email address.';
+                            } else if (!RegExp(
+                                r'^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$')
+                                .hasMatch(value)) {
+                              return 'Invalid email address format. Please enter a valid email address.';
+                            } else {
+                              return null;
+                            }
+                          }),
+                    ),
+                  ),
+                  // space
+                  const SizedBox(height: 20),
                   //phone
                   SizedBox(
                     height: 50,
@@ -141,7 +191,7 @@ class _BookingState extends State<Booking> {
                               ),
                               border: OutlineInputBorder()),
                           style: const TextStyle(
-                              fontFamily: 'Poppins', fontSize: 20),
+                              fontFamily: 'Poppins', fontSize: 16),
                           validator: (value) {
                             if (value!.isEmpty) {
                               return 'Please enter your phone number.';
@@ -166,6 +216,7 @@ class _BookingState extends State<Booking> {
                       elevation: 4,
                       shadowColor: Colors.black,
                       child: TextFormField(
+                        controller: addressController,
                           keyboardType: TextInputType.streetAddress,
                           decoration: const InputDecoration(
                               fillColor: Colors.white,
@@ -190,7 +241,7 @@ class _BookingState extends State<Booking> {
                               ),
                               border: OutlineInputBorder()),
                           style: const TextStyle(
-                              fontFamily: 'Poppins', fontSize: 20),
+                              fontFamily: 'Poppins', fontSize: 16),
                           validator: (value) {
                             if (value!.isEmpty) {
                               return 'Please enter your street address.';
@@ -320,61 +371,60 @@ class _BookingState extends State<Booking> {
                   SizedBox(
                     height: 50,
                     width: MediaQuery.of(context).size.width * 0.86,
-                    child: MouseRegion(
-                      onHover: (event) {
-                        setState(() {
-                          isHover = true;
-                        });
-                      },
-                      onExit: (event) {
-                        setState(() {
-                          isHover = false;
-                        });
-                      },
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          elevation: 5,
-                          backgroundColor: isHover
-                              ? const Color.fromARGB(247, 255, 255,
-                                  255) // White background when hovering
-                              : const Color.fromARGB(247, 84, 74,
-                                  158), // Purple background when not hovering
-                        ),
-                        onPressed: () {
-                          // Fluttertoast.showToast(
-                          //   msg: "Successfully Account create !",
-                          //   toastLength: Toast.LENGTH_SHORT,
-                          //   gravity: ToastGravity.BOTTOM,
-                          //   backgroundColor:const Color.fromARGB(247, 84, 74, 158) ,
-                          //   textColor: Colors.white,
-                          // );
-                          if (formKey.currentState!.validate()) {
+                    child:ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        elevation: 5,
+                        backgroundColor: const Color.fromARGB(247, 84, 74, 158),
+                      ),
+                      onPressed: () async {
+                        if (formKey.currentState!.validate()) {
+                          // Get the current user
+                          User? user = FirebaseAuth.instance.currentUser;
+
+                          if (user != null) {
+                            // Create a reference to the Firestore collection for bookings
+                            CollectionReference bookings = FirebaseFirestore.instance.collection('bookings');
+
+                            // Generate a unique booking id (you can use a package like 'uuid' for this)
+                            String bookingId = UniqueKey().toString();
+
+                            // Get the selected date and time from the controller
+                            String selectedDateTime = timeController.text;
+
+                            // Create a new booking document
+                            await bookings.doc(bookingId).set({
+                              'booking_id': bookingId,
+                              'date_time': selectedDateTime,
+                              'user_id': user.uid,
+                              'service_id': 'your_service_id_here', // Replace with your actual service id
+                            });
+
+                            // Navigate to the thank you screen or perform any other actions
                             Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => const ThankYou()));
+                              context,
+                              MaterialPageRoute(builder: (context) => const ThankYou()),
+                            );
                           }
-                        },
-                        child: Text(
-                          "Reserve your seat",
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontFamily: 'Poppins',
-                            color: isHover
-                                ? Colors.black45
-                                : Colors
-                                    .white, // black45 text when not hovering, white text when hovering
-                          ),
+                        }
+                      },
+                      child: const Text(
+                        "Reserve your seat",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontFamily: 'Poppins',
+                          color: Colors.white,
                         ),
                       ),
                     ),
+
                   ),
                   // Total price
                   const Padding(
                     padding: EdgeInsets.all(20.0),
                     child: Text("Total price: 600",
-                        style: TextStyle(fontSize: 30)),
+                        style: TextStyle(fontSize: 30,fontFamily: 'Poppins')),
                   ),
+
                 ],
               ),
             ),
@@ -383,5 +433,37 @@ class _BookingState extends State<Booking> {
         // bottomNavigationBar: const MyCustomBottomNavigationBar(),
       ),
     );
+  }
+
+  void fetchAndSetUserData() async {
+    try {
+      // Get the current user first
+      User? user = FirebaseAuth.instance.currentUser;
+      // Now check if the user is available or not
+      if (user != null) {
+        // Fetch the user data from Firestore using the user's UID
+        DocumentSnapshot<Map<String, dynamic>> userDataSnapshot =
+        await FirebaseFirestore.instance
+            .collection('users') // 'users' is your collection name
+            .doc(user.uid)
+            .get();
+
+        if (userDataSnapshot.exists) {
+          // Extract the user data from the snapshot
+          Map<String, dynamic> userData = userDataSnapshot.data()!;
+
+          // Set the fetched data in the text fields
+          setState(() {
+            nameController.text = userData['name'] ?? '';
+            emailController.text = userData['email'] ?? '';
+            phoneController.text = userData['phone'] ?? '';
+            addressController.text = userData['address'] ?? '';
+          });
+        }
+      }
+    } catch (e) {
+      // Handle any errors that occur during fetching the data
+      print('Error fetching user data: $e');
+    }
   }
 }
